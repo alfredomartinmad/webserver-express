968af30aff307db174a6db755f51ab17db8202a4
Alfredo <alfredo.martin@e.craftww.com>
2020-04-13 17:45:40 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+
+//Express HBS Engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        nombre: 'alfredo',
+        anio: new Date().getFullYear()
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about.hbs')
+})
+
+app.listen(port, () => {
+    console.log(`Escuhando en puerto ${port}`);
+})
